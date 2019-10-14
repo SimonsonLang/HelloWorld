bf4a10b8febde99c9dd444cca3deed09d51ca448
User/AllanY <44676663+SimonsonLang@users.noreply.github.com>
2019-10-14 11:28:32 -0700

    I made the brick show words when a button is pressed

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
 
+brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
+    brick.setStatusLight(StatusLight.GreenFlash);
+    brick.showString("Hello World", 1);
+    brick.showString("Everything is Awesome", 2);
+
+})
